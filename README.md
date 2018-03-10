dbc1c12509afb15141ccd751586c4adca0e535e0
claudia-romano <claudia.romano06@gmail.com>
2018-03-10 11:08:29 +0000

    initial commit

---
+++ README.md
@@ -0,0 +1,10 @@
+Slide In Panel
+=========
+
+A CSS3 and JavaScript powered slide-in panel, to quickly show side content, notifications or profile information.
+
+[Article on CodyHouse](https://codyhouse.co/gem/css-slide-in-panel/)
+
+[Demo](https://codyhouse.co/demo/slide-in-panel/)
+ 
+[Terms](https://codyhouse.co/terms/)
---
+++ js/main.js
@@ -0,0 +1,41 @@
+(function(){
+	var panelTriggers = document.getElementsByClassName('js-cd-panel-trigger');
+	if( panelTriggers.length > 0 ) {
+		for(var i = 0; i < panelTriggers.length; i++) {
+			(function(i){
+				var panelClass = 'js-cd-panel-'+panelTriggers[i].getAttribute('data-panel'),
+					panel = document.getElementsByClassName(panelClass)[0];
+				// open panel when clicking on trigger btn
+				panelTriggers[i].addEventListener('click', function(event){
+					event.preventDefault();
+					addClass(panel, 'cd-panel--is-visible');
+				});
+				//close panel when clicking on 'x' or outside the panel
+				panel.addEventListener('click', function(event){
+					if( hasClass(event.target, 'js-cd-close') || hasClass(event.target, panelClass)) {
+						event.preventDefault();
+						removeClass(panel, 'cd-panel--is-visible');
+					}
+				});
+			})(i);
+		}
+	}
+	
+	//class manipulations - needed if classList is not supported
+	//https://jaketrent.com/post/addremove-classes-raw-javascript/
+	function hasClass(el, className) {
+	  	if (el.classList) return el.classList.contains(className);
+	  	else return !!el.className.match(new RegExp('(\\s|^)' + className + '(\\s|$)'));
+	}
+	function addClass(el, className) {
+	 	if (el.classList) el.classList.add(className);
+	 	else if (!hasClass(el, className)) el.className += " " + className;
+	}
+	function removeClass(el, className) {
+	  	if (el.classList) el.classList.remove(className);
+	  	else if (hasClass(el, className)) {
+	    	var reg = new RegExp('(\\s|^)' + className + '(\\s|$)');
+	    	el.className=el.className.replace(reg, ' ');
+	  	}
+	}
+})();
